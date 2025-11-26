a2b4fcd9222c57269407b3f10ca01ef1b8c4d989
Michael <maikleo2019@gmail.com>
2025-11-25 23:18:37 -0500

    Actualización de bd

--- biblioteca animal.sql
+++ biblioteca animal.sql
@@ -37,12 +37,12 @@ CREATE TABLE IF NOT EXISTS `animales` (
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `animales` (`ID`, `Nombre`, `NombreCientifico`, `Clasificacion_ID`, `TiempoVidaMin`, `TiempoVidaMax`, `Habitat_ID`, `Clima_ID`, `Alimentacion_ID`, `Reproduccion_ID`, `Descripcion`, `FechaRegistro`) VALUES
-	(7, 'Le?n', 'Panthera leo', 1, 10, 14, 2, 1, 2, 3, 'Gran felino social que vive en manadas. Conocido como el rey de la selva.', '2025-11-24 22:31:22'),
+	(7, 'León', 'Panthera leo', 1, 10, 14, 2, 1, 2, 3, 'Gran felino social que vive en manadas. Conocido como el rey de la selva.', '2025-11-24 22:31:22'),
 	(9, 'Boa constrictor', 'Boa constrictor', 3, 20, 30, 6, 1, 2, 2, 'Serpiente no venenosa que mata por constricci?n.', '2025-11-24 22:32:20'),
 	(10, 'Elefante africano', 'Loxodonta africana', 1, 60, 70, 2, 1, 1, 3, 'El animal terrestre m?s grande del mundo. Vive en manadas matriarcales.', '2025-11-24 22:35:28'),
-	(11, 'Ping?ino emperador', 'Aptenodytes forsteri', 2, 15, 20, 1, 4, 5, 1, '?nico ping?ino que se reproduce durante el invierno ant?rtico. Excelente buceador.', '2025-11-24 22:35:28'),
+	(11, 'Pingüino Emperador', 'Aptenodytes forsteri', 2, 15, 20, 1, 4, 5, 1, '?nico ping?ino que se reproduce durante el invierno ant?rtico. Excelente buceador.', '2025-11-24 22:35:28'),
 	(12, 'Mariposa monarca', 'Danaus plexippus', 6, 1, 1, 1, 2, 1, 1, 'Conocida por su migraci?n masiva de hasta 4,000 km. Sus colores advierten sobre su toxicidad.', '2025-11-24 22:35:28'),
-	(13, 'Pulpo com?n', 'Octopus vulgaris', 7, 1, 2, 7, 1, 2, 1, 'Molusco muy inteligente con capacidad de cambiar color y textura para camuflarse.', '2025-11-24 22:35:28'),
+	(13, 'Pulpo común', 'Octopus vulgaris', 7, 1, 2, 7, 1, 2, 1, 'Molusco muy inteligente con capacidad de cambiar color y textura para camuflarse.', '2025-11-24 22:35:28'),
 	(14, 'Rana dorada venenosa', 'Phyllobates terribilis', 4, 10, 15, 6, 1, 4, 1, 'Uno de los animales m?s venenosos del mundo. Habita en la selva tropical colombiana.', '2025-11-24 22:35:28');
 
 CREATE TABLE IF NOT EXISTS `clasificaciones` (
@@ -54,14 +54,14 @@ CREATE TABLE IF NOT EXISTS `clasificaciones` (
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `clasificaciones` (`ID`, `Tipo`, `Subtipo`) VALUES
-	(1, 'Vertebrado', 'Mam?fero'),
+	(1, 'Vertebrado', 'Mamífero'),
 	(2, 'Vertebrado', 'Ave'),
 	(3, 'Vertebrado', 'Reptil'),
 	(4, 'Vertebrado', 'Anfibio'),
 	(5, 'Vertebrado', 'Pez'),
-	(6, 'Invertebrado', 'Artr?podo'),
+	(6, 'Invertebrado', 'Artrópodo'),
 	(7, 'Invertebrado', 'Molusco'),
-	(8, 'Invertebrado', 'An?lido'),
+	(8, 'Invertebrado', 'Anélido'),
 	(9, 'Invertebrado', 'Cnidario'),
 	(10, 'Invertebrado', 'Equinodermo');
 
@@ -75,11 +75,11 @@ CREATE TABLE IF NOT EXISTS `climas` (
 INSERT INTO `climas` (`ID`, `Clima`) VALUES
 	(1, 'Tropical'),
 	(2, 'Templado'),
-	(3, 'Des?rtico'),
+	(3, 'Desértico'),
 	(4, 'Polar'),
-	(5, 'Mediterr?neo'),
+	(5, 'Mediterráneo'),
 	(6, 'Continental'),
-	(7, 'Monta?oso');
+	(7, 'Montañoso');
 
 CREATE TABLE IF NOT EXISTS `imagenesanimales` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -108,16 +108,16 @@ INSERT INTO `subtiposhabitat` (`ID`, `TipoHabitat_ID`, `Subtipo`) VALUES
 	(3, 1, 'Desierto'),
 	(4, 1, 'Tundra'),
 	(5, 1, 'Selva tropical'),
-	(6, 1, 'Monta?a'),
-	(7, 2, 'Marino - Oc?ano'),
+	(6, 1, 'Montaña'),
+	(7, 2, 'Marino - Océano'),
 	(8, 2, 'Marino - Costa'),
-	(9, 2, 'Agua dulce - R?o'),
+	(9, 2, 'Agua dulce - Río'),
 	(10, 2, 'Agua dulce - Lago'),
 	(11, 2, 'Agua dulce - Pantano'),
 	(12, 3, 'Bosque'),
 	(13, 3, 'Sabana'),
 	(14, 3, 'Selva tropical'),
-	(15, 3, 'Monta?a');
+	(15, 3, 'Montaña');
 
 CREATE TABLE IF NOT EXISTS `tiposalimentacion` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -127,12 +127,12 @@ CREATE TABLE IF NOT EXISTS `tiposalimentacion` (
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `tiposalimentacion` (`ID`, `Tipo`) VALUES
-	(1, 'Herb?voro'),
-	(2, 'Carn?voro'),
-	(3, 'Omn?voro'),
-	(4, 'Insect?voro'),
-	(5, 'Pisc?voro'),
-	(6, 'Nectar?voro');
+	(1, 'Herbívoro'),
+	(2, 'Carnívoro'),
+	(3, 'Omnívoro'),
+	(4, 'Insectívoro'),
+	(5, 'Piscívoro'),
+	(6, 'Nectarívoro');
 
 CREATE TABLE IF NOT EXISTS `tiposhabitat` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -143,8 +143,8 @@ CREATE TABLE IF NOT EXISTS `tiposhabitat` (
 
 INSERT INTO `tiposhabitat` (`ID`, `Tipo`) VALUES
 	(1, 'Terrestre'),
-	(2, 'Acu?tico'),
-	(3, 'A?reo-terrestre');
+	(2, 'Acuático'),
+	(3, 'Aéreo-terrestre');
 
 CREATE TABLE IF NOT EXISTS `tiposreproduccion` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -154,9 +154,9 @@ CREATE TABLE IF NOT EXISTS `tiposreproduccion` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `tiposreproduccion` (`ID`, `Tipo`) VALUES
-	(1, 'Ov?paro'),
-	(2, 'Ovoviv?paro'),
-	(3, 'Viv?paro');
+	(1, 'Ovíparo'),
+	(2, 'Ovovivíparo'),
+	(3, 'Vivíparo');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
